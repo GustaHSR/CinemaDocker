3fcf4825e61acc9793a3e07da4b708b2fc594e3c
GustahsrLG <gustavo.rodrigues@lg.com.br>
2025-06-25 16:02:20 -0300

    fix: register AppService in AppModule

--- cinema-backend/src/app.module.ts
+++ cinema-backend/src/app.module.ts
@@ -5,9 +5,11 @@ import { SalasModule } from './modules/salas/salas.module';
 import { SessoesModule } from './modules/sessoes/sessoes.module';
 import { IngressosModule } from './modules/ingressos/ingressos.module';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [PrismaModule, FilmesModule, SalasModule, SessoesModule, IngressosModule],
   controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- cinema-frontend/src/pages/Sobre.js
+++ cinema-frontend/src/pages/Sobre.js
@@ -3,10 +3,10 @@ function Sobre() {
     <div className="card">
       <h1 className="title">Sobre Esta Aplicação!</h1>
       <p className="subtitle">
-        Esta aplicação foi feita por Gustavo Henrique da Silva Rodrigues, aluno de Ciência da Computação da PUC Goias!
+        Esta aplicação foi feita pelo desenvolvedor Gustavo Henrique da Silva Rodrigues!
       </p>
       <br></br>
-      <p className="subtitle">Esta aplicação foi feita em ReactJS, usando os conhecimentos adquiridos em aulas! Tecnologias como HTML, CSS e JAVASCRIPT foram amplamente utilizadas!</p>
+      <p className="subtitle">Esta é uma aplicação WEB fullstack completa com Docker utilizando tecnologias como ReactJS e Nginx para frontend e NestJS e Prisma para backend e banco de dados!</p>
     </div>
   );
 }
